c8f5914cc0a6da1d86d6e9dea7b42513d52ff7b6
At1z <147633133+At1z@users.noreply.github.com>
2024-08-15 10:32:15 +0200

    Time: 3 ms (14.05%), Space: 55.9 MB (36.83%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        if(bills[0] != 5 ){
+            return false;
+        }
+        int [ ] exchange = new int [2]; // [0] = 5, [1] = 10
+        exchange[0] = 1;
+        for(int i = 1; i < bills.length; i++){
+            if(bills[i] == 5){
+                exchange[0] += 1;
+                continue;
+            }
+            if(bills[i] == 10 && exchange[0] != 0){
+                exchange[0] -= 1;
+                exchange[1] += 1;
+                continue;
+            }
+            if(bills[i] == 20 && exchange[0] != 0 && exchange[1] != 0){
+                exchange[0] -= 1;
+                exchange[1] -= 1;
+                continue;
+            }
+            if(bills[i] == 20 && exchange[0] >= 3){
+                exchange[0] -= 3;
+                continue;
+            }
+            return false;
+        }
+        
+        return true;
+    }
+}
