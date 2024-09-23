3c25515694d3210794ba8957d61b8e54b6f6faf7
At1z <147633133+At1z@users.noreply.github.com>
2024-09-23 16:51:20 +0200

    Time: 46 ms (42.18%), Space: 44.9 MB (96.60%) - LeetHub

---
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.HashSet;
+
+class Solution {
+    public int minExtraChar(String s, String[] dictionary) {
+        int n = s.length();
+        HashSet<String> dictSet = new HashSet<>(Arrays.asList(dictionary));
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, n);
+        dp[n] = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = dp[i + 1] + 1;
+            for (int j = i + 1; j <= n; j++) {
+                String sub = s.substring(i, j);
+                if (dictSet.contains(sub)) {
+                    dp[i] = Math.min(dp[i], dp[j]);
+                }
+            }
+        }
+
+        return dp[0];
+    }
+}
