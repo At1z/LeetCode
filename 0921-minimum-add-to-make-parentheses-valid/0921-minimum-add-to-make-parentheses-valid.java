90fd0a00e19bb5f424d89000d1ab0479cacd62e5
At1z <147633133+At1z@users.noreply.github.com>
2024-10-09 10:10:57 +0200

    Time: 0 ms (100.00%), Space: 41.5 MB (28.39%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -4,16 +4,15 @@ class Solution {
         int close_needed = 0;
         for(int i = 0; i < s.length(); i++){
             if(s.charAt(i) == ')'){
-                open_needed += 1;
                 if(close_needed > 0){
-                    open_needed -= 1;
                     close_needed -= 1;
+                    continue;
                 }
+                    open_needed += 1;
             }else{
                 close_needed += 1;
             }
         }
-        int res = Math.abs(open_needed + close_needed);
-        return res;
+        return open_needed + close_needed;
     }
 }
