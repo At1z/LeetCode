f45a935e7f315f8c6b10fcb5a58f0313ae257fb5
At1z <147633133+At1z@users.noreply.github.com>
2024-10-09 10:45:09 +0200

    Delete 0197-rising-temperature directory

--- 0197-rising-temperature/0197-rising-temperature.sql
+++
@@ -1,9 +0,0 @@
-# Write your MySQL query statement below
-SELECT DISTINCT 
-    b.id AS "Id" 
-FROM 
-    Weather a, Weather b  
-WHERE  
-    b.recordDate = DATE_ADD(a.recordDate, INTERVAL 1 DAY)  
-AND 
-    b.temperature > a.temperature;
