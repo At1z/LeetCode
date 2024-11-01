4edc659578ac7a09829cc1deb9cb89c42a947c00
At1z <147633133+At1z@users.noreply.github.com>
2024-11-01 11:51:13 +0100

    Time: 34 ms (32.47%), Space: 42.6 MB (77.67%) - LeetHub

---
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string makeFancyString(string s) {
+    string result;
+    
+    for (int i = 0; i < s.size(); ++i) {
+        if (result.size() >= 2 && result[result.size() - 1] == s[i] && result[result.size() - 2] == s[i]) {
+            continue;
+        }
+        result += s[i];
+    }
+    
+    return result;
+    }
+};
