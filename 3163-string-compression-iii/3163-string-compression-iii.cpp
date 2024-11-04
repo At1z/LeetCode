4b4d774991b8aabe001a5f3fda3db1247c20d717
At1z <147633133+At1z@users.noreply.github.com>
2024-11-04 09:29:19 +0100

    Time: 23 ms (76.07%), Space: 29.5 MB (74.87%) - LeetHub

--- 3163-string-compression-iii/3163-string-compression-iii.cpp
+++ 3163-string-compression-iii/3163-string-compression-iii.cpp
@@ -3,16 +3,20 @@ public:
     string compressedString(string word) {
         int i = 0;
         int counter = 1;
-        string res = "";
-        while (i < word.length()) {
-            if (i + 1 == word.length() || word[i] != word[i + 1] || counter > 8) {
-                res += to_string(counter) + word[i];
+        stringstream ss;
+        while(i < word.length()){
+            if(i+1 == word.length()){
+                ss << counter << word[i];
+                break;
+            }
+            if (word[i] != word[i+1] || counter > 8){
+                ss << counter << word[i];
                 counter = 1;
-            } else {
+            }else{
                 counter += 1;
             }
             i++;
         }
-        return res;
+        return ss.str();
     }
 };
