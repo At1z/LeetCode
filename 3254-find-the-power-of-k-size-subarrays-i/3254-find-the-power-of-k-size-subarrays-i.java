505c6a059ca1bdecb3855adb20e50ca99fcef36e
At1z <147633133+At1z@users.noreply.github.com>
2024-11-16 09:37:41 +0100

    Time: 2 ms (58.78%), Space: 45.3 MB (63.56%) - LeetHub

---
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] resultsArray(int[] nums, int k) {
+        int n = nums.length;
+        int[] subA = new int[n - k + 1];
+        for(int i = n-1; i > k-2; i--){
+            boolean consecutive = true;
+            for(int j = i; j > i - (k - 1); j--){
+                if((nums[j] - 1) != nums[j-1]){
+                    consecutive = false;
+                    break;
+                }
+            }
+            if(consecutive ){
+                subA[i-k+1] = nums[i];
+            }else{
+               subA[i-k+1] = -1; 
+            }
+        }
+        return subA;
+    }
+}
