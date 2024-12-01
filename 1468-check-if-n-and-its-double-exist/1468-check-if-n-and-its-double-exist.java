97297444e3ec4a37b7678174ebb12147cd02e46b
At1z <147633133+At1z@users.noreply.github.com>
2024-12-01 08:41:38 +0100

    Time: 3 ms (33.07%), Space: 43.3 MB (21.02%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
@@ -2,6 +2,13 @@ class Solution {
     public boolean checkIfExist(int[] arr) {
         HashMap<Integer,Integer> doubles = new HashMap<>();
         for(int num:arr){
+            if(doubles.containsKey(num*2)){
+                if(num == 0 && doubles.get(0) < 2){
+                    doubles.put(num,doubles.getOrDefault(num, 0)+1);
+                    continue;
+                }
+                return true;
+            }
             doubles.put(num,doubles.getOrDefault(num, 0)+1);
         }
         for(int num:doubles.keySet()){
