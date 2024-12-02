3e83d643b4cb2d3b4c189478c487fa1531b41d82
At1z <147633133+At1z@users.noreply.github.com>
2024-12-02 09:34:30 +0100

    Time: 0 ms (100%), Space: 41.1 MB (73.72%) - LeetHub

--- 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.java
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.java
@@ -3,7 +3,7 @@ class Solution {
         String[] prefix = sentence.split(" ");
         for(int i = 0; i < prefix.length; i++){
             if(prefix[i].length() >= searchWord.length() && prefix[i].substring(0,searchWord.length()).equals(searchWord)){
-                    return i+1 ;
+                return i+1 ;
             }
         }
         return -1;
