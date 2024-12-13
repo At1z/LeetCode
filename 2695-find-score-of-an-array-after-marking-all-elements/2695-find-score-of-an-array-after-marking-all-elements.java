a4af078236058b705b60cb5068cdd4a378dcceb3
At1z <147633133+At1z@users.noreply.github.com>
2024-12-13 10:28:42 +0100

    Time: 180 ms (45.59%), Space: 57.1 MB (89.81%) - LeetHub

---
+++ 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.java
@@ -0,0 +1,33 @@
+class Solution {
+    public long findScore(int[] nums) {
+        long res = 0;
+        boolean[] visited = new boolean[nums.length];
+
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((arr1, arr2) -> {
+            if (arr1[0] != arr2[0]) return arr1[0] - arr2[0];
+            return arr1[1] - arr2[1];
+        });
+
+        for (int i = 0; i < nums.length; i++) {
+            minHeap.add(new int[] { nums[i], i });
+        }
+
+        while (!minHeap.isEmpty()) {
+            int[] element = minHeap.remove();
+            int number = element[0];
+            int index = element[1];
+            if (!visited[index]) {
+                res += number;
+                visited[index] = true;
+                if (index - 1 >= 0) {
+                    visited[index - 1] = true;
+                }
+                if (index + 1 < nums.length) {
+                    visited[index + 1] = true;
+                }
+            }
+        }
+
+        return res;
+    }
+}
