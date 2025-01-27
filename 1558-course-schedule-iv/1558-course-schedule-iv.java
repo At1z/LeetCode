6152dde695099873bc0ffd890615c9e3f404b8d6
At1z <147633133+At1z@users.noreply.github.com>
2025-01-27 09:52:27 +0100

    Time: 37 ms (70%), Space: 48.4 MB (45.88%) - LeetHub

--- 1558-course-schedule-iv/1558-course-schedule-iv.java
+++ 1558-course-schedule-iv/1558-course-schedule-iv.java
@@ -1,12 +1,5 @@
 class Solution {
     public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        for (int i = 0; i < numCourses; i++) {
-            graph.put(i, new ArrayList<>());
-        }
-        for (int[] prerequisite : prerequisites) {
-            graph.get(prerequisite[0]).add(prerequisite[1]);
-        }
         boolean[][] reachable = new boolean[numCourses][numCourses];
         for (int[] prerequisite : prerequisites) {
             reachable[prerequisite[0]][prerequisite[1]] = true;
