db83c4c7d0598145fa4d5af3082ec65b498704e4
At1z <147633133+At1z@users.noreply.github.com>
2025-02-23 18:27:07 +0100

    Time: 0 ms (100%), Space: 43.4 MB (67.41%) - LeetHub

---
+++ 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.java
@@ -0,0 +1,21 @@
+class Solution {
+    int preIndex = 0, postIndex = 0;
+
+    public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
+        return build(preorder, postorder);
+    }
+
+    private TreeNode build(int[] preorder, int[] postorder) {
+        TreeNode root = new TreeNode(preorder[preIndex++]);
+
+        if (root.val != postorder[postIndex]) { 
+            root.left = build(preorder, postorder);
+        }
+        if (root.val != postorder[postIndex]) {
+            root.right = build(preorder, postorder);
+        }
+
+        postIndex++; 
+        return root;
+    }
+}
