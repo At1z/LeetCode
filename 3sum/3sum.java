61df24c23702815f8cb596b49b11a3818e673b56
At1z <147633133+At1z@users.noreply.github.com>
2025-02-24 12:31:33 +0100

    Time: 33 ms, Memory: 52 MB - LeetHub

--- 3sum/3sum.java
+++ 3sum/3sum.java
@@ -4,6 +4,7 @@ class Solution {
         //[-1,0,1,2,-1,-4,3]
         Arrays.sort(nums); 
         //[-4,-1,-1,0,1,2,3]
+         Set<List<Integer>> seen = new HashSet<>();
         for(int i = 0; i < nums.length; i++){
             if(i > 0 && nums[i] == nums[i-1]){
                 continue;
@@ -12,21 +13,21 @@ class Solution {
             int h = nums.length - 1;
             while(l < h){
                 if((nums[i] + nums[l] + nums[h]) == 0){
-                    res.add(Arrays.asList(nums[i], nums[l], nums[h]));
-                    while(h > l && nums[h] == nums[h-1]){
-                        h -= 1;
-                    }
-                    while(h > l && nums[l] == nums[l+1]){
-                        l += 1;
+                    List<Integer> triplet = Arrays.asList(nums[i], nums[l], nums[h]);
+                    if (seen.add(triplet)) {
+                        res.add(triplet);
                     }
                     l += 1;
                     h -= 1;
-                }else if((nums[i] + nums[l] + nums[h]) > 0){
-                    h -= 1;
+                    
                 }
-                else{
+                while((nums[i] + nums[l] + nums[h]) > 0 && h > l){
+                    h -= 1;
+                } 
+                while((nums[i] + nums[l] + nums[h]) < 0 && h > l){
                     l += 1;
                 }
+                
             }
         }
         return res;
