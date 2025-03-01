e7b404b98d9d1df5cf547390aeeef5328dcdb6f7
At1z <147633133+At1z@users.noreply.github.com>
2025-03-01 14:36:08 +0100

    Time: 2 ms (8.49%), Space: 44.5 MB (8.68%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
@@ -1,25 +1,28 @@
 class Solution {
     public int[] applyOperations(int[] nums) {
-        Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] == nums[i + 1] && nums[i] != 0) {
-                nums[i] *= 2;  // Double the number
-                nums[i + 1] = 0; // Set next to zero
+        Queue<Integer> num = new LinkedList<>();
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] == nums[i-1] && nums[i-1] != 0){
+                num.add(nums[i-1] * 2);
+                nums[i] = 0;
+            }else if(nums[i] != nums[i-1]){
+                if(nums[i-1] != 0){
+                    num.add(nums[i-1]);
+                }
             }
-        }
-        for (int num : nums) {
-            if (num != 0) {
-                queue.add(num);
+            if(nums.length-1 == i){
+                if(nums[i] != 0){
+                    num.add(nums[i]);
+                }
             }
         }
         int index = 0;
-        while (!queue.isEmpty()) {
-            nums[index++] = queue.poll();
+        while(!num.isEmpty()){
+            nums[index++] = num.poll();
         }
-        while (index < nums.length) {
+        while(index < nums.length){
             nums[index++] = 0;
         }
-
         return nums;
     }
 }
