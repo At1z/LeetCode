bd57b983a2a76f14e04abf8a7c7a0b279490903f
At1z <147633133+At1z@users.noreply.github.com>
2025-03-01 14:33:55 +0100

    Time: 2 ms (8.49%), Space: 44.3 MB (17.11%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
@@ -1,7 +1,6 @@
 class Solution {
     public int[] applyOperations(int[] nums) {
         Queue<Integer> num = new LinkedList<>();
-        Queue<Integer> zero = new LinkedList<>();
         for(int i = 1; i < nums.length; i++){
             if(nums[i] == nums[i-1] && nums[i-1] != 0){
                 num.add(nums[i-1] * 2);
@@ -9,16 +8,10 @@ class Solution {
             }else if(nums[i] != nums[i-1]){
                 if(nums[i-1] != 0){
                     num.add(nums[i-1]);
-                }else{
-                    zero.add(0);
                 }
-            }else{
-                zero.add(0);
             }
             if(nums.length-1 == i){
-                if(nums[i] == 0){
-                    zero.add(0);
-                }else{
+                if(nums[i] != 0){
                     num.add(nums[i]);
                 }
             }
@@ -27,8 +20,8 @@ class Solution {
         while(!num.isEmpty()){
             nums[index++] = num.poll();
         }
-        while(!zero.isEmpty()){
-            nums[index++] = zero.poll();
+        while(index < nums.length){
+            nums[index++] = 0;
         }
         return nums;
     }
