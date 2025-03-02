7b61321096864a3b92e7e92a2c09a15e4c769878
At1z <147633133+At1z@users.noreply.github.com>
2025-03-02 11:55:33 +0100

    Time: 1 ms (29.98%), Space: 42.5 MB (15.41%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,24 +1,14 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int p1 = m - 1; 
-        int p2 = n - 1; 
-        int p = m + n - 1; 
-
-        while (p1 >= 0 && p2 >= 0) {
-            if (nums1[p1] > nums2[p2]) {
-                nums1[p] = nums1[p1];
-                p1--;
-            } else {
-                nums1[p] = nums2[p2];
-                p2--;
-            }
-            p--;
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for(int i = 0; i < m; i++){
+            minHeap.add(nums1[i]);
         }
-        while (p2 >= 0) {
-            nums1[p] = nums2[p2];
-            p--;
-            p2--;
+        for(int i = 0; i < n; i++){
+            minHeap.add(nums2[i]);
         }
-
+        for(int i = 0; i < nums1.length; i++){
+            nums1[i] = minHeap.poll();
+        }
     }
 }
