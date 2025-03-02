b870cde4a7e3e8aa3c3ab23a33846eb595860283
At1z <147633133+At1z@users.noreply.github.com>
2025-03-02 11:51:54 +0100

    Time: 0 ms (100%), Space: 44.7 MB (96.71%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.java
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.java
@@ -1,5 +1,37 @@
 class Solution {
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
+        int n1 = nums1.length - 1;
+        int n2 = nums2.length - 1;
+        List<int[]> merge = new ArrayList<>();
+        int p1 = 0;
+        int p2 = 0;
+
+        while (p1 <= n1 && p2 <= n2) {
+            if (nums1[p1][0] == nums2[p2][0]) {
+                merge.add(new int[]{nums1[p1][0], nums1[p1][1] + nums2[p2][1]}); // Fixed the issue
+                p1 += 1;
+                p2 += 1;
+            } else if (nums1[p1][0] < nums2[p2][0]) {
+                merge.add(nums1[p1]);
+                p1 += 1;
+            } else {
+                merge.add(nums2[p2]);
+                p2 += 1;
+            }
+        }
+
+        while (p1 <= n1) {
+            merge.add(nums1[p1]);
+            p1 += 1;
+        }
+
+        while (p2 <= n2) {
+            merge.add(nums2[p2]);
+            p2 += 1;
+        }
+
+        return merge.toArray(new int[merge.size()][]);
+        /*
         Map<Integer, Integer> values = new TreeMap<>();
         for(int[] num : nums1){
             values.put(num[0], values.getOrDefault(num[0],0) + num[1]);
@@ -14,5 +46,6 @@ class Solution {
             res[index++][1] = values.get(key);
         }
         return res;
+        */
     }
 }
