7d1ef632bfcd2bcc0323cdeb7bd2180db74ce26b
At1z <147633133+At1z@users.noreply.github.com>
2025-03-05 13:32:06 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return root;
+        }
+        TreeNode newRoot = new TreeNode(root.val);
+        newRoot.left = new TreeNode(root.right.val);
+        leftSide(root.right, newRoot.left);
+        newRoot.right = new TreeNode(root.left.val);
+        rightSide(root.left, newRoot.right);
+        return newRoot;
+    }
+    public void leftSide(TreeNode toor, TreeNode newRoot){
+        System.out.println(toor.val);
+        if(toor.right != null){
+            newRoot.left = new TreeNode(toor.right.val);
+            leftSide(toor.right, newRoot.left);
+        }
+        if(toor.left != null){
+            newRoot.right = new TreeNode(toor.left.val);
+            leftSide(toor.left, newRoot.right);
+        }
+    }
+
+    public void rightSide(TreeNode toor, TreeNode newRoot){
+        System.out.println(toor.val);
+        if(toor.left != null){
+            newRoot.right = new TreeNode(toor.left.val);
+            rightSide(toor.left, newRoot.right);
+        }
+        if(toor.right != null){
+            newRoot.left = new TreeNode(toor.right.val);
+            rightSide(toor.right, newRoot.left);
+        }
+    }
+}
