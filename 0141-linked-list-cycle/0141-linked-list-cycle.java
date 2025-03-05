623b76476c458524ff7ce418da471932f96e58a2
At1z <147633133+At1z@users.noreply.github.com>
2025-03-05 17:39:51 +0100

    Time: 0 ms (100%), Space: 44.2 MB (87.87%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,7 +11,18 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if (head == null) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+        /*if (head == null) {
             return false;
         }
         HashSet<ListNode> seen = new HashSet<>();
@@ -22,6 +33,7 @@ public class Solution {
             seen.add(head);
             head = head.next;
         }
-        return false;  
+        return false;
+        */  
     }
 }
