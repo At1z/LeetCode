c9e2adf8f92ff605aa1174fd7df704f1c53fb698
At1z <147633133+At1z@users.noreply.github.com>
2025-03-07 13:12:33 +0100

    Time: 91 ms (68.4%), Space: 50 MB (46.4%) - LeetHub

--- 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
@@ -1,5 +1,46 @@
 class Solution {
     public int[] closestPrimes(int left, int right) {
+        boolean[] isPrime = sieve(right);
+        List<Integer> primes = new ArrayList<>();
+        for (int i = Math.max(2, left); i <= right; i++) {
+            if (isPrime[i]) {
+                primes.add(i);
+            }
+        }
+        if (primes.size() < 2) {
+            return new int[]{-1, -1}; 
+        }
+
+        int minDist = Integer.MAX_VALUE;
+        int prime1 = -1, prime2 = -1;
+
+        for (int i = 1; i < primes.size(); i++) {
+            int diff = primes.get(i) - primes.get(i - 1);
+            if (diff < minDist) {
+                minDist = diff;
+                prime1 = primes.get(i - 1);
+                prime2 = primes.get(i);
+            }
+        }
+
+        return new int[]{prime1, prime2};
+    }
+
+    private boolean[] sieve(int n) {
+        boolean[] isPrime = new boolean[n + 1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false;  
+
+        for (int i = 2; i * i <= n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        return isPrime;
+    }        
+        /*
         List<Integer> primes = new ArrayList<>();
         for(int i = left; i <= right; i++){
             if(isPrimal(i)){
@@ -23,7 +64,9 @@ class Solution {
         }
 
         return new int[]{prime1, prime2};
-        /*
+        
+        //another way
+
         int num1 = -1;
         int num2 = -1;
         int dist = Integer.MAX_VALUE;
@@ -49,7 +92,8 @@ class Solution {
             }
         }
         return (num2 == -1)? new int[]{-1,-1}: new int[]{num1,num2};
-        */
+
+        // en here
     }
     public boolean isPrimal(int number){
         if(number <= 1){
@@ -62,4 +106,5 @@ class Solution {
         }
         return true;
     }
+    */
 }
