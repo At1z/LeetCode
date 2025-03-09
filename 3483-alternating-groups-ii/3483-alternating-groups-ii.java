238c2d2c6905d4cc2f8fc76db2e83a1e30656379
At1z <147633133+At1z@users.noreply.github.com>
2025-03-09 11:27:09 +0100

    Time: 45 ms (7.26%), Space: 58 MB (84.08%) - LeetHub

---
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numberOfAlternatingGroups(int[] colors, int k) {
+        int[] extendedArr = new int[colors.length + (k - 1)];
+        int res = 0;
+        System.arraycopy(colors, 0, extendedArr, 0, colors.length);
+        System.arraycopy(colors, 0, extendedArr, colors.length, k - 1);
+        System.out.println(Arrays.toString(extendedArr));
+        int counter = 1;
+        for(int i = 1; i < extendedArr.length; i++){
+            if(extendedArr[i] != extendedArr[i-1]){
+                counter += 1;
+            }else{
+                counter = 1;
+            }
+            if(counter == k){
+                res += 1;
+                counter -= 1;
+            }
+        }
+        return res;
+    }
+}
