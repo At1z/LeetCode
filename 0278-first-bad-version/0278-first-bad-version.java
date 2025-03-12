87dacde7a60a35c0a1d1981ef0620011256fdadf
At1z <147633133+At1z@users.noreply.github.com>
2025-03-12 18:40:02 +0100

    Time: 26 ms (26.32%), Space: 40.1 MB (91.3%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -5,16 +5,14 @@ public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int l = 1;
         int r = n;
-        int res = 0;
         while(l <= r){
             int mid = l + (r - l)/2;
             if(isBadVersion(mid)){
-                res = mid;
                 r = mid - 1;
             }else{
                 l = mid + 1;
             }
         }
-        return res;
+        return l;
     }
 }
