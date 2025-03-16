ebfbe8cae7e928b7eb53c09bbc4e5150d687f3f4
At1z <147633133+At1z@users.noreply.github.com>
2025-03-16 13:47:30 +0100

    Time: 17 ms (87.1%), Space: 56.6 MB (32.53%) - LeetHub

--- 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.java
+++ 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.java
@@ -1,7 +1,6 @@
 class Solution {
     public long repairCars(int[] ranks, int cars) {
         /// (200:4)^(1/2) = (50)^(1/2) ~= 7 
-        long res = 0;
         long l = 1;
         long r = 0;
         for(int i = 0; i < ranks.length; i++){
@@ -18,12 +17,11 @@ class Solution {
             }
 
             if(counter >= cars){
-                res = mid;
                 r = mid - 1;
             }else{
                 l = mid + 1;
             }
         }
-        return res;
+        return l;
     }
 }
