bc9f87939aa5c75a0bde0b8525ff1ccf10eaafe7
At1z <147633133+At1z@users.noreply.github.com>
2025-03-22 14:05:10 +0100

    Time: 27 ms (14.38%), Space: 45.3 MB (45.72%) - LeetHub

--- 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.java
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.java
@@ -1,46 +1,56 @@
 class Solution {
     Map<Integer,List<Integer>> graph = new HashMap<>();
-    Set<Integer> seen = new HashSet<>();
+    int[] seen;
+
     public int countCompleteComponents(int n, int[][] edges) {
         int res = 0;
+        for(int i=0;i<n;i++){
+            graph.put(i ,new ArrayList<>());
+        } 
         graphBuilder(edges);
+        seen = new int[n];
+
         for(int node : graph.keySet()){
-            if(seen.contains(node)){
+            if (seen[node] == 1){
                 continue;
             }
-            seen.add(node);
+            
+            seen[node] = 1;
             Set<Integer> allNodes = new HashSet<>(graph.get(node));
             allNodes.add(node);
-            if(isCompleted(graph.get(node), allNodes.size() - 1, allNodes )){
+
+            if(isCompleted(graph.get(node), allNodes)){
                 res += 1;
             }
         }
-        return res + n - seen.size();
+        return res + (n - Arrays.stream(seen).sum());
     }
 
-    public void graphBuilder(int[][] edges){
-        for(int[] edge : edges){
-            graph.putIfAbsent(edge[0],new ArrayList<>());
-            graph.putIfAbsent(edge[1],new ArrayList<>());
-            graph.get(edge[0]).add(edge[1]);
-            graph.get(edge[1]).add(edge[0]);
-        }
-    }
+    public boolean isCompleted(List<Integer> connections, Set<Integer> allNodes){
+        int numberOfNodes = allNodes.size() - 1;
 
-    public boolean isCompleted(List<Integer> connections, int numberOfNodes, Set<Integer> allNodes){
         for(int node : connections){
             List<Integer> curConnections = graph.get(node);
             if(numberOfNodes != curConnections.size()){
                 return false;
             }
+            
             for(int edge : curConnections){
-                if(!allNodes.contains(edge)){
-                    seen.add(edge);
+                if(!allNodes.contains(edge) ){
+                    seen[edge] = 1;
                     return false;
                 }
             }
-            seen.add(node);
+
+            seen[node] = 1;
         }
         return true;
+    }
+
+    public void graphBuilder(int[][] edges){
+        for(int[] edge : edges){
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }
     }
 }
