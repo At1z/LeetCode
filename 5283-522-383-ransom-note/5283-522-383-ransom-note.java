a775229f89810539762c8b71d0a4f792d1a28440
At1z <147633133+At1z@users.noreply.github.com>
2025-03-25 17:31:18 +0100

    Time: 2 ms, Memory: 44.5 MB - LeetHub

---
+++ 5283-522-383-ransom-note/5283-522-383-ransom-note.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        Set<Integer> seen = new HashSet<>();
+        char[] ransomFreq = new char[26];
+        char[] magazineFreq = new char[26];
+        int curRansomIndex = 0;
+        int curMagazineIndex = 0;
+        while(curRansomIndex < ransomNote.length() && curMagazineIndex < magazine.length()){
+            ransomFreq[ransomNote.charAt(curRansomIndex) -'a'] += 1;
+            magazineFreq[magazine.charAt(curMagazineIndex) -'a' ] += 1;
+            curRansomIndex += 1;
+            curMagazineIndex += 1;
+        }
+        while(curRansomIndex < ransomNote.length()){
+            ransomFreq[ransomNote.charAt(curRansomIndex) -'a'] += 1;
+            curRansomIndex += 1;            
+        }
+        while(curMagazineIndex < magazine.length()){
+            magazineFreq[magazine.charAt(curMagazineIndex) -'a' ] += 1;
+            curMagazineIndex += 1;
+        }
+        for(int i = 0; i < ransomNote.length(); i++){
+            int curLetter = ransomNote.charAt(i) -'a';
+            if(seen.contains(curLetter)){
+                continue;
+            }
+            seen.add(curLetter);
+            if(ransomFreq[curLetter] > magazineFreq[curLetter] ){
+                return false;
+            }
+        }
+        return true;
+    }
+}
