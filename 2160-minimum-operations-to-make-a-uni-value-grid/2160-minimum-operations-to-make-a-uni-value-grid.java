f8a314fe1c0118613aac1f6a5d572d1f81521966
At1z <147633133+At1z@users.noreply.github.com>
2025-03-26 09:36:17 +0100

    Time: 126 ms (8.54%), Space: 74.5 MB (63.32%) - LeetHub

--- 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.java
+++ 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.java
@@ -7,18 +7,14 @@ class Solution {
             }
         }
         Collections.sort(nums);
-        
+
         int remainder = nums.get(0) % x;
         for (int num : nums) {
             if (num % x != remainder) {
-                return -1;  // Not possible
+                return -1;  
             }
         }
-        
-        // Find the median
         int median = nums.get(nums.size() / 2);
-        
-        // Compute the number of operations to make all elements equal to the median
         int operations = 0;
         for (int num : nums) {
             operations += Math.abs(num - median) / x;
