a644c94f77aa512b3605c028a68551ce5f700b1c
At1z <147633133+At1z@users.noreply.github.com>
2025-04-01 08:30:04 +0200

    Time: 39 ms (9.64%), Space: 128.9 MB (5.53%) - LeetHub

---
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public long mostPoints(int[][] questions) {
+        Map<Integer, Long> memo = new HashMap<>();
+        return solve(questions, 0, memo);
+    }
+
+    public long solve(int[][] questions, int index, Map<Integer, Long> memo) {
+        if (index >= questions.length) {
+            return 0;
+        }
+        
+        if (memo.containsKey(index)) {
+            return memo.get(index);
+        }
+        
+        int[] curQuestion = questions[index];
+        long take = curQuestion[0] + solve(questions, index + curQuestion[1] + 1, memo);
+        long skip = solve(questions, index + 1, memo);
+        
+        long result = Math.max(take, skip);
+        memo.put(index, result);
+        return result;
+    }
+}
