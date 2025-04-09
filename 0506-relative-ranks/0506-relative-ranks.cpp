7e379c1f6dd75aeda07a056957d064b30877a438
At1z <147633133+At1z@users.noreply.github.com>
2025-04-09 16:24:34 +0200

    Time: 14 ms (11.58%), Space: 17.5 MB (36.83%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.cpp
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     vector<string> findRelativeRanks(vector<int>& score) {
-        vector<int> copyScore = score;
-        map<int, int> mapOfIndex;
-        vector<string> res;
-
-        std::sort(copyScore.begin(), copyScore.end(), std::greater<int>());
-
-        for (int i = 0; i < copyScore.size(); i++) {
-            mapOfIndex[copyScore[i]] = i + 1; 
+    vector<int> copyScore = score;
+    map<int,int> mapOfIndex;
+    vector<string> res;
+    std::sort(copyScore.begin(), copyScore.end());
+    for(int i = 0; i < copyScore.size(); i++){
+        mapOfIndex[copyScore[i]] = copyScore.size() - i  ;
+    }
+    for(int sc : score){
+        string curRes = std::to_string(mapOfIndex[sc]);
+        if(curRes == "1"){
+            res.push_back("Gold Medal");
+        }else if(curRes == "2"){
+            res.push_back("Silver Medal");
+        }else if (curRes == "3"){
+            res.push_back("Bronze Medal");
+        }else{
+            res.push_back(curRes);
         }
+    }
 
-        for (int sc : score) {
-            int rank = mapOfIndex[sc];
-            if (rank == 1) {
-                res.push_back("Gold Medal");
-            } else if (rank == 2) {
-                res.push_back("Silver Medal");
-            } else if (rank == 3) {
-                res.push_back("Bronze Medal");
-            } else {
-                res.push_back(std::to_string(rank));
-            }
-        }
 
-        return res;
+    return res;
     }
 };
