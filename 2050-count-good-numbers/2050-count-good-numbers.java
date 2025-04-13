f3d362e913fb067b25e0a25f5c981583d4a24243
At1z <147633133+At1z@users.noreply.github.com>
2025-04-13 12:14:06 +0200

    Time: 0 ms (100%), Space: 41 MB (9.67%) - LeetHub

---
+++ 2050-count-good-numbers/2050-count-good-numbers.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int countGoodNumbers(long n) {
+        int mod = 1_000_000_007;
+        
+        long even = (n + 1) / 2; 
+        long odd = n / 2;        
+
+        long power5 = modPow(5, even, mod);
+        long power4 = modPow(4, odd, mod);
+
+        return (int)((power5 * power4) % mod);
+    }
+
+    private long modPow(long base, long exp, int mod) {
+        long result = 1;
+        base %= mod;
+        while (exp > 0) {
+            if ((exp & 1) == 1)
+                result = (result * base) % mod;
+            base = (base * base) % mod;
+            exp >>= 1;
+        }
+        return result;
+    }
+}
