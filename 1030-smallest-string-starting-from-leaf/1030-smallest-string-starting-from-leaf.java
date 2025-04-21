dc9f2a39afa6b33b56362fc87ffb6d1ef269ffda
At1z <147633133+At1z@users.noreply.github.com>
2025-04-21 23:19:49 +0200

    Time: 9 ms (29.48%), Space: 45 MB (60.99%) - LeetHub

---
+++ 1030-smallest-string-starting-from-leaf/1030-smallest-string-starting-from-leaf.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public String smallestFromLeaf(TreeNode root) {
+        if(root == null){
+            return "";
+        }
+        List<String> res = new ArrayList<>();
+        backtracking(root, new StringBuilder(), res);
+        System.out.println(res);
+        Collections.sort(res);
+        return res.get(0);
+    }
+    public void backtracking(TreeNode root, StringBuilder curWord, List<String> res){
+        curWord.append((char) ('a' + root.val));
+        if(root.left == null && root.right == null){
+            
+            res.add(new StringBuilder(curWord).reverse().toString());
+            return;
+        }
+        if(root.left != null){
+            backtracking(root.left,curWord,res);
+            curWord.setLength(curWord.length() - 1);
+        }
+        if(root.right != null){
+            backtracking(root.right,curWord,res);
+            curWord.setLength(curWord.length() - 1);
+        }
+        
+    }
+}
