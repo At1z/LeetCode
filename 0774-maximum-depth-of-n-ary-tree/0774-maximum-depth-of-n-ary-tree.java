a4a4439b4d41405115bd9afc60f9ab67e8fc3e84
At1z <147633133+At1z@users.noreply.github.com>
2025-04-21 18:09:05 +0200

    Time: 0 ms (100%), Space: 43.8 MB (88.27%) - LeetHub

---
+++ 0774-maximum-depth-of-n-ary-tree/0774-maximum-depth-of-n-ary-tree.java
@@ -0,0 +1,39 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public int maxDepth(Node root) {
+        if(root == null){
+            return 0;
+        }
+        return dfs(root,1);
+    }
+    public int dfs(Node root, int level){
+        List<Node> curNodeList = root.children;
+        if(curNodeList.size() == 0){
+            return level;
+        }
+        int maxLevel = 0;
+        for(Node child : curNodeList){
+            maxLevel = Math.max(maxLevel, dfs(child,level + 1));
+        }
+
+        return maxLevel;
+    }
+}
