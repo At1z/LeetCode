dbcce442e5cb2632e7fa5cd0f3ab11ac9ac07b25
At1z <147633133+At1z@users.noreply.github.com>
2025-05-08 08:58:25 +0200

    Time: 195 ms (80.62%), Space: 108.8 MB (69.16%) - LeetHub

---
+++ 3628-find-minimum-time-to-reach-last-room-ii/3628-find-minimum-time-to-reach-last-room-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    int[][] dir = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
+    public int minTimeToReach(int[][] moveTime) {
+        int rows = moveTime.length;
+        int cols = moveTime[0].length;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((room1, room2) -> room1[2]-room2[2] );
+        int[][] visited = new int[rows][cols];
+        pq.add(new int[]{0,0,0,0}); // row, col, value
+        visited[0][0] = 1;
+        while(!pq.isEmpty()){
+            int[] curRoom = pq.poll();
+            if(curRoom[0] == rows - 1 && curRoom[1] == cols - 1){
+                return curRoom[2];
+            }
+            for(int[] d:dir){
+                if(curRoom[0] + d[0] >= 0 && curRoom[0] + d[0] < rows && curRoom[1] + d[1] >= 0 && curRoom[1] + d[1] < cols && visited[curRoom[0] + d[0]][curRoom[1] + d[1]] != 1 ){
+                    visited[curRoom[0] + d[0]][curRoom[1] + d[1]] = 1;
+                    if(curRoom[3] % 2 == 0){
+                        pq.add(new int[]{curRoom[0] + d[0], curRoom[1] + d[1], Math.max(curRoom[2] + 1, moveTime[curRoom[0] + d[0]][curRoom[1] + d[1]] + 1),curRoom[3] + 1 });
+                    }else{
+                        pq.add(new int[]{curRoom[0] + d[0], curRoom[1] + d[1], Math.max(curRoom[2] + 2, moveTime[curRoom[0] + d[0]][curRoom[1] + d[1]] + 2),curRoom[3] + 1 });
+                    }    
+                }
+            }
+            
+        }
+
+        return -1;
+    }
+}
