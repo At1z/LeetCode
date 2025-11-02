0c346ea1600d151775c9c68bee1f6e2e558447ef
At1z <147633133+At1z@users.noreply.github.com>
2025-11-02 14:46:07 +0100

    Time: 16 ms (55.23%), Space: 47.4 MB (5.86%) - LeetHub

--- 2317-count-collisions-on-a-road/2317-count-collisions-on-a-road.java
+++ 2317-count-collisions-on-a-road/2317-count-collisions-on-a-road.java
@@ -8,17 +8,45 @@ class Solution {
         for(int i = 0; i < directionsTab.length; i++){
             if(directionsTab[i] == 'R'){
                 rightDirCars += 1;
-            }
-            if(directionsTab[i] == 'L'){
+                if(leftDirCars > 0){
+                    collisionCounter += leftDirCars + rightDirCars;
+                    rightDirCars = 0;
+                    leftDirCars = 0;
+                    lastStayingCar = i;
+                }
+                if(leftDirCars == 0 && lastStayingCar == i){
+                    collisionCounter += rightDirCars;
+                    lastStayingCar = i;
+                    rightDirCars = 0;
+                }
+            }else if(directionsTab[i] == 'L'){
                 leftDirCars += 1;
-            }
-            if(directionsTab[i] == 'S'){
+                if(rightDirCars > 0){
+                    collisionCounter += leftDirCars + rightDirCars;
+                    rightDirCars = 0;
+                    leftDirCars = 0;
+                    lastStayingCar = i;
+                }
+                if(rightDirCars == 0){
+                    if(lastStayingCar != -1 && lastStayingCar < i){
+                        collisionCounter += 1;
+                        lastStayingCar = i;
+                    }
+                    leftDirCars = 0;
+                }                
+            }else if(directionsTab[i] == 'S'){
                 lastStayingCar = i;
+                if(rightDirCars > 0 && leftDirCars == 0){
+                    collisionCounter += rightDirCars;
+                    lastStayingCar = i;
+                    rightDirCars = 0;
+                }
             }
             //System.out.println(directionsTab[i]);
             //System.out.println("before r " + rightDirCars + " l " + leftDirCars + " s " + lastStayingCar + " c " + collisionCounter);
+            /*
             if(rightDirCars > 0 && leftDirCars > 0){
-                collisionCounter += 2 + rightDirCars - 1;
+                collisionCounter += leftDirCars + rightDirCars;
                 rightDirCars = 0;
                 leftDirCars = 0;
                 lastStayingCar = i;
@@ -30,13 +58,12 @@ class Solution {
                 }
                 leftDirCars = 0;
             }
-            if(rightDirCars > 0 && leftDirCars == 0){
-                if(lastStayingCar != -1 && lastStayingCar == i){
+            if(rightDirCars > 0 && leftDirCars == 0 && lastStayingCar == i){
                     collisionCounter += rightDirCars;
                     lastStayingCar = i;
                     rightDirCars = 0;
-                }
             }
+            */
             //System.out.println("after r " + rightDirCars + " l " + leftDirCars + " s " + lastStayingCar + " c " + collisionCounter);
             //System.out.println();
             
